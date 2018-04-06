5a88230523885657ac0ba7d40dae410a000405af
Mark Zsombor <markszsombor@gmail.com>
2018-04-06 12:35:13 -0700

    altered the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="navbar">
+    <div className="navbar navbar-fixed-top">
       <a href="/" className="navbar-brand">Mark's 5 Day Weather</a>
     </div>
   );
