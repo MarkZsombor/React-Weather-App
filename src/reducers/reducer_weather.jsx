b4712f1bb7a8278da5c295491809f07b9a4256c9
Mark Zsombor <markszsombor@gmail.com>
2018-04-06 18:23:02 -0700

    invalid city name no longer crashes the app

--- src/reducers/reducer_weather.jsx
+++ src/reducers/reducer_weather.jsx
@@ -4,7 +4,9 @@ export default function (state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
       // return state.concat([action.payload.data]);
-      return [action.payload.data, ...state];
+      if (action.payload.data) {
+        return [action.payload.data, ...state];
+      }
   }
   return state;
 }
